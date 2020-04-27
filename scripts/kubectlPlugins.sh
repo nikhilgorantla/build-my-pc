de54174e5ca633a6823d06e4b65b549ad2a1c297
Nikhil <35358177+nikhilgorantla@users.noreply.github.com>
2020-04-27 01:41:50 -0400

    Kubectl plugins

---
+++ scripts/kubectlPlugins.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+## CLI TOOLS 
+
+kubectl krew install ctx
+kubectl krew install ns
+kubectl krew install access-matrix
+kubectl krew install exec-as
+kubectl krew install prompt
+kubectl krew install ingress-nginx
+kubectl krew install neat
+kubectl krew install node-shell
+kubectl krew install outdated
+kubectl krew install pod-dive
+kubectl krew install rbac-view
+kubectl krew install resource-capacity
+kubectl krew install resource-snapshot
+kubectl krew install rolesum
+kubectl krew install sniff
+kubectl krew install status
+kubectl krew install tree
+kubectl krew install view-allocations
+kubectl krew install view-secret
+
